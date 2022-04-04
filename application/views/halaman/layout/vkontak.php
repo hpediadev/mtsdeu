3bc0a42bca0741a2c58a77a81b4497552cdae7bb
hpediadev <hpedia.dev@gmail.com>
2022-04-04 16:44:05 +0700

    kjaj

--- application/views/halaman/layout/vkontak.php
+++ application/views/halaman/layout/vkontak.php
@@ -10,7 +10,8 @@
                         </div>
 					
 								<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1980.2838477163425!2d113.5607223!3d-6.9421554!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd9cf7792deab95%3A0x8302802cc08cb4ca!2sSMK%20PUTRA%20BANGSA!5e0!3m2!1sid!2sid!4v1641305283105!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.7373118655833!2d113.48348311392235!3d-7.040127194914976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd9d16459f40103%3A0x4037908dd4bc7980!2sMTs.%20Miftahul%20Ulum%20MDK!5e0!3m2!1sid!2sid!4v1642371384030!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+								<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.7373118655833!2d113.48348311392235!3d-7.040127194914976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd9d16459f40103%3A0x4037908dd4bc7980!2sMTs.%20Miftahul%20Ulum%20MDK!5e0!3m2!1sid!2sid!4v1642371384030!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
+								<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15842.215440816464!2d113.550864!3d-6.943799!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x772033cba2086f69!2sMTs%20Swasta%20Darul%20Ulum%202!5e0!3m2!1sid!2sid!4v1649065346554!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 									<br><br>
 									
