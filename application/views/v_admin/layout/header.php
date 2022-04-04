9e8ab424774abe7193887af7a218ca7f4994c9b3
hpediadev <hpedia.dev@gmail.com>
2022-04-04 16:51:45 +0700

    kjaj

--- application/views/v_admin/layout/header.php
+++ application/views/v_admin/layout/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Halaman Admin | MTs. Miftahul Ulum</title>
+  <title>Halaman Admin | MTs. Darul Ulum II</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
    <link rel="icon" href="<?= base_url('logo.png')?>" type="image/png" sizes="16x16">
@@ -41,7 +41,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>MTs</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>MTs Miftahul Ulum</b></span>
+      <span class="logo-lg"><b>MTs Darul Ulum II</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-fixed-top">
