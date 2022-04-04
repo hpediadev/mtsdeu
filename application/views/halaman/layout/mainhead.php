ef137675dee2b7bda5431c5d3e3dbea9f3a1abf7
hpediadev <hpedia.dev@gmail.com>
2022-04-04 21:28:45 +0700

    Assets

--- application/views/halaman/layout/mainhead.php
+++ application/views/halaman/layout/mainhead.php
@@ -11,7 +11,7 @@
                                     <p>Info Pembekalan Tahun Praktik Kerja Lapangan akan dilakukan hari senin12 Juni 2022, Informasi Lebih Lanjut <a href="" style="background-color: <?= $warna2?>; padding: 2px;color: white;" id="warna2"><b> Klik Disini </b></a></p>
                                 </div> -->
                                 
-                                    <marquee>Selamat Datang di MTS. Miftahul Ulum Pegantenan Pamekasan, Segera Bergabun dengan Kami</marquee>
+                                    <marquee>Selamat Datang di MTS. Darul Ulum II Waru Barat Waru Pamekasan, Segera Bergabung dengan Kami</marquee>
                             </div>
                              <!-- <div class="item">
                                 <div class="tg-description">
